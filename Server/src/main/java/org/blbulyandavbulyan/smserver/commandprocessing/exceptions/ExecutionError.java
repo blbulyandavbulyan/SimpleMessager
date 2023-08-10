703453b70306082a16c8ab43185262151efe7216
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-10 14:09:00 +0400

    removed CommandProcessor and ExecutionError, because these classes are unused

--- Server/src/main/java/org/blbulyandavbulyan/smserver/commandprocessing/exceptions/ExecutionError.java
+++
@@ -1,8 +0,0 @@
-package org.blbulyandavbulyan.smserver.commandprocessing.exceptions;
-
-public class ExecutionError extends CommandProcessingException{
-    public ExecutionError(Exception e){
-        super(e);
-    }
-
-}
