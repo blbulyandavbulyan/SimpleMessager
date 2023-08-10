9835ab1ff8eb9b82503bf49dbfa92e1f4ab78396
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-10 14:15:16 +0400

    replaced printStackTrace to logger

--- Server/src/main/java/org/blbulyandavbulyan/smserver/threads/ClientProcessingServerThread.java
+++ Server/src/main/java/org/blbulyandavbulyan/smserver/threads/ClientProcessingServerThread.java
@@ -8,70 +8,64 @@ import org.blbulyandavbulyan.smgeneral.message.Message;
 import org.blbulyandavbulyan.smgeneral.message.servercommand.ServerCommand;
 import org.blbulyandavbulyan.smgeneral.message.servermessages.ServerErrorMessage;
 
-public class ClientProcessingServerThread extends ClientServerThread{
+public class ClientProcessingServerThread extends ClientServerThread {
     private final String clientName;
 
-    public ClientProcessingServerThread(ClientServerThread clientServerThread, String clientName){
+    public ClientProcessingServerThread(ClientServerThread clientServerThread, String clientName) {
         super(clientServerThread);
         this.clientName = clientName;
         start();
     }
+
     synchronized public void sendMessage(Message msg) throws IOException {
-        if(!isTerminated()){
+        if (!isTerminated()) {
             clientObjOut.writeObject(msg);
         }
     }
-    public void run(){
-        try{
+
+    public void run() {
+        try {
             clientObjOut.writeUTF("WELCOME TO SERVER!");
             clientObjOut.flush();
             serverLogger.info("Поток для клиента %s запущен\n".formatted(clientName));
-            while(!isTerminated()){
-                try{
+            while (!isTerminated()) {
+                try {
                     Message msg = (Message) clientObjIn.readObject();
-                    if(!msg.getSender().equals(clientName)){
+                    if (!msg.getSender().equals(clientName)) {
                         sendMessage(new ServerErrorMessage(clientName, msg.getSender(), ServerErrorMessage.ServerErrorCode.YOUR_USERNAME_ON_SERVER_AND_IN_YOUR_MESSAGE_ARE_NOT_EQUAL));
-                    }
-                    else if(msg.getReceiver()!= null){
+                    } else if (msg.getReceiver() != null) {
                         String msgReceiver = msg.getReceiver();
-                        if(msgReceiver.equalsIgnoreCase("SERVER")){
-                            if(msg instanceof ServerCommand){
-                                //todo добавить обработку команды
-
-                            }
+                        if (msgReceiver.equalsIgnoreCase("SERVER") && msg instanceof ServerCommand) {
+                            //todo добавить обработку команды
+                            ServerCommand serverCommand = (ServerCommand)msg;
+                            serverLogger.info("Пришла команда от " + clientName + ", содержание: " + serverCommand.getCommand());
+                        } else if (!Objects.equals(msgReceiver, clientName)) {
+                            if (server.IsClientExists(msgReceiver)) server.getClient(msgReceiver).sendMessage(msg);
+                            else
+                                sendMessage(new ServerErrorMessage(clientName, msgReceiver, ServerErrorMessage.ServerErrorCode.MESSAGE_DELIVERY_ERROR_NO_USER_WITH_THIS_NAME));
                         }
-                        else if(!Objects.equals(msgReceiver, clientName)){
-                            if(server.IsClientExists(msgReceiver)) server.getClient(msgReceiver).sendMessage(msg);
-                            else sendMessage(new ServerErrorMessage(clientName, msgReceiver, ServerErrorMessage.ServerErrorCode.MESSAGE_DELIVERY_ERROR_NO_USER_WITH_THIS_NAME));
-                        }
-                    }
-                    else{
+                    } else {
                         for (ClientProcessingServerThread client : server.getClients()) {
-                            if(client != this)client.sendMessage(msg);
+                            if (client != this) client.sendMessage(msg);
                         }
                     }
-                }
-                catch(ClassNotFoundException | ClassCastException e){
-
-                    e.printStackTrace();
+                } catch (ClassNotFoundException | ClassCastException e) {
+                    serverLogger.error("Ошибка при работе с клиентом " + clientName, e);
                 }
             }
-        }
-        catch (SocketException e){
-            if(!(e.getMessage().equals("Socket closed") && isTerminated())){
-                e.printStackTrace();
+        } catch (SocketException e) {
+            if (!(e.getMessage().equals("Socket closed") && isTerminated())) {
+                serverLogger.error("Ошибка при работе с сокетом, имя клиента " + clientName, e);
             }
-        }
-        catch(EOFException  e){
+        } catch (EOFException e) {
             serverLogger.info("Пользователь %s отключился.\n".formatted(clientName));
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        finally {
+        } catch (IOException e) {
+            serverLogger.error("Ошибка ввода вывода при работе с клиентом " + clientName, e);
+        } finally {
             server.removeClient(clientName);
         }
     }
+
     public String getClientName() {
         return clientName;
     }
